c5ab1352465bf3206ddd456deb65fdef7aa03036
akhkilkumar <38779286+akhkilkumar@users.noreply.github.com>
2025-06-06 16:11:31 +0530

    Update bash_script_to_run_gmx_cluster_on_mulitple_file.bash
    added command

--- MD/bash_script_to_run_gmx_cluster_on_mulitple_file.bash
+++ MD/bash_script_to_run_gmx_cluster_on_mulitple_file.bash
@@ -1,1 +1,2 @@
+for xtc in all_replica_combine_*.xtc; do echo $xtc ; echo 4  4 | gmx cluster -f "$xtc" -s mtb_avg_all_replica_${xtc:20:-4}.pdb -cl "${xtc:20:-4}_cluster.pdb" -cutoff 0.2 -sz "${xtc:20:-4}_clust-size.xvg" -g "${xtc:20:-4}_cluster.log";  done
 
